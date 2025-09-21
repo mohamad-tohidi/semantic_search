61ad4c319c3c69f077a556d43176a3c84fc694d2
Tohidi <the.mohammad.tohidi@gmail.com>
2025-09-21 19:05:12 +0330

    activate cuda

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def main():
     for model_name in MODEL_NAMES:
         print(f"\n--- Loading and benchmarking model: {model_name} ---")
         model = SentenceTransformer(model_name)
-        model.device("cuda")
+        model.to(device="cuda")
         tokenizer = AutoTokenizer.from_pretrained(model_name)
 
         results_for_model = {}
